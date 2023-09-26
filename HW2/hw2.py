874c9d4342cef048f8c4b54a2d33f6e2ff565100
Siddarth <pullakhandam.siddartha@gmail.com>
2023-09-25 21:06:07 -0500

    Update hw2.py

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -1,15 +1,49 @@
 def q1(roots):
-    polynomial = [1]
-
+    # Finding the roots of the polynomia
+    poly = [1]
     for root in roots:
-        new_polynomial = [0] * (len(polynomial) + 1)
-        for i in range(len(polynomial)):
-            new_polynomial[i] += polynomial[i]
-            new_polynomial[i + 1] -= root * polynomial[i]
-        polynomial = new_polynomial
+        new_poly = [0] * (len(poly) + 1)
+        for i in range(len(poly)):
+            new_poly[i] += poly[i]
+            new_poly[i + 1] -= root * poly[i]
+        poly = new_poly
 
-    return polynomial
+    return poly
 
+#print(q1([2,3]))
+
+
+def q2(coefficients):
+    # Step (a): Find the factors for k
+    k = coefficients[-1]
+    lst = []
+    for i in range(1, abs(k) + 1):
+        # Check if k is evenly divisible by i
+        if k % i == 0:
+            # If it is, add i to the list lst
+            lst.append(i)
+
+    # Initialize a list to store the unique roots
+    unique_roots = []
+
+    # Step (b): checking if the result is zero or not if it's zero then that is the factor
+    for c in lst:
+        result = 0
+        for coef in coefficients:
+            result = result * c + coef
+
+        if result == 0:
+            unique_roots.append(c)
+        else:
+            new_lst = []
+            for x in lst:
+                if x - c != 0 and result % (x - c) == 0:
+                    new_lst.append(x)
+            lst = new_lst
+
+    return unique_roots
 
 # Test cases
-print(q1([2, 3])) 
+print(q2([1, -5, 6]))
+print(q2([1, -12, 47, -60]))
+
